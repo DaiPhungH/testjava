73fbab8df57397578577a91d0e56dd6585e19b68
Đại Phùng Hữu <daiphung79@gmail.com>
2024-10-09 13:17:01 +0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner nhap = new Scanner(System.in);
+        System.out.print("Nhap so day: ");
+        int i = nhap.nextInt();
+        Scanner nhap2 = new Scanner(System.in);
+        System.out.print("Nhap so vao day: ");
+        int y = nhap.nextInt();
+        System.out.printf("Giá trị : %d, %d",i,y);
+
+    }
+    }
